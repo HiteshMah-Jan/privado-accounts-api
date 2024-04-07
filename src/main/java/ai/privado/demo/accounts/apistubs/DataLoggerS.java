0301aef00b9f0556eb16c5b44f2df4f3eb89ace3
Vishesh <45128646+vishesh27k@users.noreply.github.com>
2024-04-07 12:51:49 +0530

    Update DataLoggerS.java

--- src/main/java/ai/privado/demo/accounts/apistubs/DataLoggerS.java
+++ src/main/java/ai/privado/demo/accounts/apistubs/DataLoggerS.java
@@ -1,7 +1,6 @@
 package ai.privado.demo.accounts.apistubs;
 
 import java.io.IOException;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
